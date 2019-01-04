8e6eaf3cbac079f7850684d78d8a538c7fa63add
nl <3210346136@qq.com>
2019-01-04 15:08:13 +0800

    Fix bug crash for FIPS problem

--- HashChecker/Properties/AssemblyInfo.cs
+++ HashChecker/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("https://github.com/nl8590687/HashChecker")]
 [assembly: AssemblyProduct("HashChecker")]
-[assembly: AssemblyCopyright("Copyright ©  2017 nl8590687")]
+[assembly: AssemblyCopyright("Copyright ©  2017-2019 nl8590687")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.1")]
-[assembly: AssemblyFileVersion("1.1.0.1")]
+[assembly: AssemblyVersion("1.2.0.2")]
+[assembly: AssemblyFileVersion("1.2.0.2")]
